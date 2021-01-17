7218c173b4d4d5683b3755578d400f335aaa2076
danaharley <danaharley.github@gmail.com>
2021-01-17 23:49:16 +0700

    json server, fetch dynamic data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My New Website",
-      body: "Lorem ipsum ....",
-      author: "Restiana",
-      id: 1,
-    },
-    {
-      title: "My First Articles",
-      body: "Lorem ipsum ....",
-      author: "Dana",
-      id: 2,
-    },
-    {
-      title: "My Second Articles",
-      body: "Lorem ipsum ....",
-      author: "Harliansyah",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     setBlogs(blogs.filter((blog) => blog.id !== id));
   };
 
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`can't fetch the data`);
+        }
+        return res.json();
+      })
+      .then((result) => {
+        setBlogs(result);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="home">
+      {error && <div className="error">{error}</div>}
+      {isLoading && <div className="loading">Loading...</div>}
       <BlogList blogs={blogs} handleDelete={handleDelete} title="All Blogs" />
     </div>
   );
