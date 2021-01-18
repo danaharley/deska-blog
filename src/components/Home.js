7b6d7326181d4f9391aa6909fb75db726c252cb2
danaharley <danaharley.github@gmail.com>
2021-01-18 19:17:11 +0700

    useFetch hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { data: blogs, isLoading, error, setData } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   const handleDelete = (id) => {
-    setBlogs(blogs.filter((blog) => blog.id !== id));
+    setData(blogs.filter((blog) => blog.id !== id));
   };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`can't fetch the data`);
-        }
-        return res.json();
-      })
-      .then((result) => {
-        setBlogs(result);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
-      });
-  }, []);
 
   return (
     <div className="home">
---
+++ src/components/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`can't fetch the data`);
+        }
+        return res.json();
+      })
+      .then((result) => {
+        setData(result);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { data, isLoading, error, setData };
+};
+
+export default useFetch;
